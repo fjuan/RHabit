4de05433738c905d3f932986ab03586f39485151
Francisco de Juan <francisco.juan@gmail.com>
2013-08-18 11:03:39 +0200

    use pluran names on navigation

--- config/navigation.rb
+++ config/navigation.rb
@@ -1,6 +1,6 @@
 SimpleNavigation::Configuration.run do |navigation|  
   navigation.items do |primary|
-    primary.item :payments,   'Payment',    payments_path
+    primary.item :payments,   'Payments',   payments_path
     primary.item :meals,      'Meals',      meals_path
     primary.item :chronicles, 'Chronicles', chronicles_path
 
