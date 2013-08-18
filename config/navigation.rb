f8fb635de04c0165647e83d5c811c2d75dc0e33b
Francisco de Juan <francisco.juan@gmail.com>
2013-08-18 11:06:32 +0200

    highlight navigation on subpaths

--- config/navigation.rb
+++ config/navigation.rb
@@ -1,8 +1,8 @@
 SimpleNavigation::Configuration.run do |navigation|  
   navigation.items do |primary|
-    primary.item :payments,   'Payments',   payments_path
-    primary.item :meals,      'Meals',      meals_path
-    primary.item :chronicles, 'Chronicles', chronicles_path
+    primary.item :payments,   'Payments',   payments_path,    highlights_on: :subpath
+    primary.item :meals,      'Meals',      meals_path,       highlights_on: :subpath
+    primary.item :chronicles, 'Chronicles', chronicles_path,  highlights_on: :subpath
 
     primary.dom_class = 'nav'
   end
