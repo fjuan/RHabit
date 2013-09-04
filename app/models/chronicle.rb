797b030e2df0687234f8c778a264cea23d006246
Francisco de Juan <francisco.juan@gmail.com>
2013-09-04 07:51:45 +0200

    scope uniqueness of date chronicles by user

--- app/models/chronicle.rb
+++ app/models/chronicle.rb
@@ -2,7 +2,7 @@ class Chronicle < ActiveRecord::Base
 
   STATES_OF_MIND = %w(happy angry sad worried)
 
-  validates :date, presence: true, uniqueness: true
+  validates :date, presence: true, uniqueness: {scope: :user_id}
   validates :mood, :summary, presence: true
 
   belongs_to :user
